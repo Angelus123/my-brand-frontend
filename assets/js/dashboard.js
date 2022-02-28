12f8908f2f73cd0927b3399f8e4e50d0aea89c85
Angelus123 <izerefaifelix@gmail.com>
2022-02-28 07:54:35 +0200

    Work on Admin dashboard

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,12 +1,4 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBWPkObUFhbBsDWm9C4tUuRFedoXhR5LIg",
-    authDomain: "my-brand-6d710.firebaseapp.com",
-    projectId: "my-brand-6d710",
-    storageBucket: "my-brand-6d710.appspot.com",
-    messagingSenderId: "290071395795",
-    appId: "1:290071395795:web:a2681620feeec77c908326"
-  };
-  firebase.initializeApp(firebaseConfig);
+
 const formLogout = () =>{
     firebase.auth().signOut().then(() => {
         location.href = 'index.html';
@@ -17,10 +9,223 @@ const formLogout = () =>{
     })
     }
 
-    function openBlogForm() {
-        document.getElementById("myBlogForm").style.display = "block";
+      function openBlogForm() {
+          document.getElementById("myBlogForm").style.display = "block";
+        }
+      function openBlogEditForm(id) {
+        var docRef = db.collection("blogs").doc(id);
+              docRef.get().then((doc) => {
+                  if (doc.exists) {
+                    let objData = doc.data()
+                      document.getElementById("myBlogEditForm").style.display = "block";
+                      document.getElementById("pic").innerHTML=`<img src=${objData.Url} alt="Blog" height=150px width=200px>`
+                      document.getElementById("blog-edit-id").setAttribute("value", `${id}`);
+                      document.getElementById("title-edit-field").setAttribute("value", `${objData.title}`);
+                      // document.getElementById("article-edit-text").setAttribute("value", `${objData.article}`);
+                  } else {
+                      // doc.data() will be undefined in this case
+                      console.log("No such document!");
+                  }
+              }).catch((error) => {
+                  console.log("Error getting document:", error);
+              });
+        console.log("id: ", id)
+      }
+
+      function closeBlogEditForm() {
+        document.getElementById("myBlogEditForm").style.display = "none";
+      }
+
+      function closeBlogForm() {
+        document.getElementById("myBlogForm").style.display = "none";
+      }
+
+      function openMessageForm() {
+        document.getElementById("myMessageForm").style.display = "block";
+      }
+      function closeMessageForm() {
+        document.getElementById("myMessageForm").style.display = "none";
       }
     
       function closeLoginForm() {
         document.getElementById("myBlogForm").style.display = "none";
       }
+
+      const firebaseConfig = {
+        apiKey: "AIzaSyBWPkObUFhbBsDWm9C4tUuRFedoXhR5LIg",
+        authDomain: "my-brand-6d710.firebaseapp.com",
+        projectId: "my-brand-6d710",
+        storageBucket: "my-brand-6d710.appspot.com",
+        messagingSenderId: "290071395795",
+        appId: "1:290071395795:web:a2681620feeec77c908326"
+      };
+      
+    const app=firebase.initializeApp(firebaseConfig);
+
+    const db = app.firestore();
+    const deleteData = (id) => {
+      console.log("id: ", id)
+      db.collection("blogs").doc(id).delete().then(() => {
+        console.log("Document successfully deleted!");
+        location.reload();
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    }
+
+    const editData = (id) => {
+      console.log("id: ", id)
+      db.collection("blogs").doc(id).edit().then(() => {
+        console.log("Document successfully deleted!");
+        location.reload();
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    }
+    const data = [];
+    db.collection("blogs")
+    .get()
+    .then((querySnapshot) => {
+          let  preData ={}
+        querySnapshot.forEach((doc) => {
+          preData = doc.data()
+          preData.id = doc.id
+          
+
+            data.push(preData)
+            console.log("Data:", preData);
+        });
+        data.map(rowdata => {
+                      // Find a <table> element with id="myTable":
+                      var table = document.getElementById("myTable");
+
+                      // Create an empty <tr> element and add it to the 1st position of the table:
+                      var row = table.insertRow(1);
+
+                      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+                      var cell1 = row.insertCell(0);
+                      var cell2 = row.insertCell(1);
+                      var cell3 = row.insertCell(2);
+
+                      // Add some text to the new cells:
+                      cell1.innerHTML = `${rowdata.title}`;
+                      cell2.innerHTML = `${rowdata.article}`;
+                      cell3.innerHTML = `<button onclick=deleteData('${rowdata.id}')> 
+                                          DELETE 
+                                         </button>
+                                         <a href="#myBlogEditForm"<button onclick=openBlogEditForm('${rowdata.id}')> EDIT</button></a>`;
+
+        })
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+const formAddBlog = document.querySelector(".form-add-blog");
+formAddBlog.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  const blog__imgurl = document.getElementById("photo").files[0]
+  const imageName = blog__imgurl.name
+  const blogRef = app.storage().ref(`Images/${imageName}`)
+  const uploadTask = blogRef.put(blog__imgurl)
+  const title =  formAddBlog.title.value
+  const article =  formAddBlog.article.value
+  const BlogText = document.querySelector(".blog-text");
+  const spinner = document.querySelector(".lds-ring");
+  const percent = document.querySelector("#percent");
+  spinner.style.display = "block";
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+       console.log("Upload is " + progress + "% done")
+       if(progress<=99) {
+        percent.innerHTML = `${ Math.trunc(progress)}%`;
+        percent.style.marginLeft = "48%";
+        spinner.style.display = "inline-block";
+       }
+       else {
+        
+        percent.style.display = "none";
+        spinner.style.display = "none";
+       }
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.paused:
+          console.log("uplaoding paused")
+          break
+        case firebase.storage.TaskState.running:
+          console.log("uplaod is running")
+      }
+    },
+    (error) => {
+      console.log(error)
+    },
+    () => {
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadedImage) => {
+        db.collection("blogs")
+          .add({
+            title: title,
+            Url: downloadedImage,
+            article: article,
+            like: [],
+            disLike: [],
+            createdAt: Date.now(),
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+          .then((blogs) => {
+            console.log(blogs)
+            location.reload();
+          })
+          .catch((error) => {
+            console.log(error)
+            location.reload();
+          })
+      })
+    }
+  )
+ 
+}
+)
+
+const formEditBlog = document.querySelector(".form-edit-blog");
+formEditBlog.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  console.log("Hello")
+  console.log(formEditBlog.id.value)
+
+})
+
+
+//manage message
+    db.collection("message")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.data().id = doc.id;
+
+            data.push(doc.data())
+            // console.log(doc.data());
+        });
+        data.map(rowdata => {
+                      // Find a <table> element with id="myTable":
+                      var table = document.getElementById("myTableMessage");
+
+                      // Create an empty <tr> element and add it to the 1st position of the table:
+                      var row = table.insertRow(1);
+
+                      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+                      var cell1 = row.insertCell(0);
+                      var cell2 = row.insertCell(1);
+                      var cell3 = row.insertCell(2);
+
+                      // Add some text to the new cells:
+                      cell1.innerHTML = `${rowdata.name}`;
+                      cell2.innerHTML = `${rowdata.message}`;
+                      cell3.innerHTML = `<a href="#myMessageForm"><button onclick=deleteData(8)> DELETE </button><button style="font-size: 16px; color: green; font-weight: 300; margin: 10px;" onclick="openMessageForm()">REPLY</button></a>`;
+
+        })
+    })
+    .catch((error) => {
+    });
+
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,108 @@
 function openLoginForm() {
-    document.getElementById("myLoginForm").style.display = "block";
-  }
+  document.getElementById("myLoginForm").style.display = "block";
+}
 
-  function closeLoginForm() {
-    document.getElementById("myLoginForm").style.display = "none";
-  }
-  function openSignUpForm() {
-    document.getElementById("mySignUpForm").style.display = "block";
+function closeLoginForm() {
+  document.getElementById("myLoginForm").style.display = "none";
+}
+function openSignUpForm() {
+  document.getElementById("mySignUpForm").style.display = "block";
+}
 
-  }
- 
-  function closeSignUpForm() {
-    document.getElementById("mySignUpForm").style.display = "none";
-  }
-  function openReadMoreForm() {
-    console.log("Hellooooo")
-    document.getElementById("readMore").style.display = "block";
-    location.href = '#readMore';
+function closeSignUpForm() {
+  document.getElementById("mySignUpForm").style.display = "none";
+}
+function openReadMoreForm() {
+  document.getElementById("readMore").style.display = "block";
+  location.href = "#readMore";
+}
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  function myMap() {
-    var mapProp = {
-      center: new google.maps.LatLng(51.508742, -0.120850),
-      zoom: 5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-  }
+}
 
-  function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+const form = document.querySelector(".form-container");
+const formSignup = document.querySelector(".form-container-signup");
+const formMessage = document.querySelector(".form-container-message");
+const formComment = document.querySelector(".comment-form")
+const firebaseConfig = {
+  apiKey: "AIzaSyBWPkObUFhbBsDWm9C4tUuRFedoXhR5LIg",
+  authDomain: "my-brand-6d710.firebaseapp.com",
+  projectId: "my-brand-6d710",
+  storageBucket: "my-brand-6d710.appspot.com",
+  messagingSenderId: "290071395795",
+  appId: "1:290071395795:web:a2681620feeec77c908326",
+};
+const app = firebase.initializeApp(firebaseConfig);
 
-  const form = document.querySelector('.form-container')
-  const formSignup = document.querySelector('.form-container-signup')
-  const formMessage = document.querySelector('.form-container-message')
-  const firebaseConfig = {
-    apiKey: "AIzaSyBWPkObUFhbBsDWm9C4tUuRFedoXhR5LIg",
-    authDomain: "my-brand-6d710.firebaseapp.com",
-    projectId: "my-brand-6d710",
-    storageBucket: "my-brand-6d710.appspot.com",
-    messagingSenderId: "290071395795",
-    appId: "1:290071395795:web:a2681620feeec77c908326"
-  };
-  const app=firebase.initializeApp(firebaseConfig);
+formComment.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(formComment._id.value) 
+    console.log(formComment.comment.value)
+    console.log(formComment.name.value);
 
-form.addEventListener('submit', (e)=>{
-  console.log("Loged In")
-  e.preventDefault()
+  db.collection("comment")
+    .add({
+      article_id: formComment._id.value,
+      comment: formComment.comment.value,
+      name: formComment.name.value
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+      location.href = "#myCommentTable";
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+});
+
+form.addEventListener("submit", (e) => {
+  console.log("Loged In");
+  e.preventDefault();
   // Initialize Firebase
-  
-  app.auth().signInWithEmailAndPassword(form.email.value, form.password.value)
-  .then((userCredential) => {
-    console.log("Logged In")
-    location.href = 'dashboard.html';
 
-    // Signed in
-    var user = userCredential.user;
-    console.log("Logged In: ", user)
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorMessage)
-  });
-})
-
-formSignup.addEventListener('submit', (e)=>{
-  e.preventDefault()
-  console.log("signUp In: ",formSignup.email.value, "signUp In: ",formSignup.password.value)
-  // Initialize Firebase
-  app.auth().createUserWithEmailAndPassword(formSignup.email.value, formSignup.password.value)
+  app
+    .auth()
+    .signInWithEmailAndPassword(form.email.value, form.password.value)
     .then((userCredential) => {
-      // Signed in 
-      
+      console.log("Logged In");
+      location.href = "dashboard.html";
+
+      // Signed in
+      var user = userCredential.user;
+      console.log("Logged In: ", user);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
+
+formSignup.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(
+    "signUp In: ",
+    formSignup.email.value,
+    "signUp In: ",
+    formSignup.password.value
+  );
+  // Initialize Firebase
+  app
+    .auth()
+    .createUserWithEmailAndPassword(
+      formSignup.email.value,
+      formSignup.password.value
+    )
+    .then((userCredential) => {
+      // Signed in
+
       var user = userCredential.user;
       // ...
     })
@@ -86,23 +110,103 @@ formSignup.addEventListener('submit', (e)=>{
       var errorCode = error.code;
       var errorMessage = error.message;
       // ..
-    })
-  })
-  const db = app.firestore();
-
-  formMessage .addEventListener('submit', (e)=>{
-    e.preventDefault()
-    console.log(formMessage.name.value, formMessage.message.value)
-  
-  db.collection("message").add({
-    name: formMessage.name.value,
-    message: formMessage.message.value,
-
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
+    });
 });
+const db = app.firestore();
+function readMore(id, title, article, image) {
+  console.log(id, title, article, image);
+  const form = document.getElementById("readMore");
+  form.style.display ="block"
+  db.collection("comment").where("article_id", "==", id)
+    .get()
+    .then((querySnapshot) => {
+      
+        querySnapshot.forEach((doc) => {
+
+          var table = document.getElementById("myCommentTable");
+
+          // Create an empty <tr> element and add it to the 1st position of the table:
+          var row = table.insertRow(1);
+    
+          // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+          var cell1 = row.insertCell(0);
+    
+          // Add some text to the new cells:
+          cell1.innerHTML = `${doc.data().comment}`
+         
+                 
+        }); 
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    }
+    );
+  document.getElementById("readmore-image").innerHTML=`<img src=${image} alt="Blog" height=400px width=70%>`
+  document.getElementById("readmore-title").innerHTML=`${title}`
+  document.getElementById("comment-id").setAttribute("value", `${id}`);
+  document.getElementById("readmore-article").innerHTML=`${article}`
+  location.href = "#readMore";
+
+}
+
+formMessage.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(formMessage.name.value, formMessage.message.value);
+
+  db.collection("message")
+    .add({
+      name: formMessage.name.value,
+      message: formMessage.message.value,
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
+});
+
+const data = [];
+let preData = {};
+db.collection("blogs")
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      preData = doc.data();
+      preData.id = doc.id;
+      data.push(preData);
+      // console.log(doc.data());
+    });
+    data.forEach(function (rowdata) {
+      console.log(rowdata.title);
+      // Find a <table> element with id="myTable":
+      var table = document.getElementById("myBlogTable");
+
+      // Create an empty <tr> element and add it to the 1st position of the table:
+      var row = table.insertRow(1);
+
+      // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+      var cell1 = row.insertCell(0);
+
+      // Add some text to the new cells:
+      cell1.innerHTML = `
+                    <div class="blog-info">
+                    <img src="${rowdata.Url}" alt="" srcset=""
+                      class="blog-img">
+                    <h2>${rowdata.title}</h2>
+                    
+                    <button class="read-more" onclick="readMore('${rowdata.id}','${rowdata.title}','${rowdata.article}','${rowdata.Url}')">Read more</button>
+                    <span class="blog-date">Jan 21, 2022 • 8 min read</span>
+                    <div class="options">
+                      <span><img src="./assets/img/icon/comments.png" alt="" style="height: 25px ; width: 35px"
+                          srcset=""></span>
+                      <span><img src="./assets/img/icon/like.png" alt="" style="height: 25px ; width: 35px" srcset=""></span>
+                      <span><img src="./assets/img/icon/unlike.png" alt="" style="height: 25px ; width: 35px" srcset=""></span>
+                      <span><img src="./assets/img/icon/share.png" alt="" style="height: 25px ; width: 35px" srcset=""></span>
+                    </div>
+                  </div>`;
+    });
   })
+  .catch((error) => {
+    console.log("Error getting documents: ", error);
+  });
